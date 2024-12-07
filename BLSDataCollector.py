8e22d3889a3d5e224db92c41d3a8d7a11797e617
RynoV12 <vilterryan@gmail.com>
2024-12-07 12:14:12 -0600

    Update BLSDataCollector.py (added data caching decorators)

--- BLSDataCollector.py
+++ BLSDataCollector.py
@@ -4,60 +4,70 @@ import pandas as pd
 import requests
 import json
 
-dataframes_dict = {}
-headers = {'Content-type': 'application/json'}
-
 # Retrieval of series data
-data = json.dumps({"seriesid": ['LNS12000000', 'LNS13000000', 'LNS14000000', 'CES0000000001'],"startyear":"2023", "endyear":"2024"})
-p = requests.post('https://api.bls.gov/publicAPI/v2/timeseries/data/', data=data, headers=headers)
-json_data = json.loads(p.text)
+@st.cache_data
+def collect_bls_data():
+    headers = {'Content-type': 'application/json'}
+    data = json.dumps({"seriesid": ['LNS12000000', 'LNS13000000', 'LNS14000000', 'CES0000000001'],"startyear":"2023", "endyear":"2024"})
+    p = requests.post('https://api.bls.gov/publicAPI/v2/timeseries/data/', data=data, headers=headers)
+    json_data = json.loads(p.text)
+    return json_data
 
 # Iterate over the JSON response and extract data from each inputted series
-for series in json_data['Results']['series']:
-    series_id = series['seriesID']
-    parsed_data = []
+@st.cache_data
+def process_bls_data(json_data):
+    dataframes_dict = {}
+    for series in json_data['Results']['series']:
+        series_id = series['seriesID']
+        parsed_data = []
 
-    for item in series['data']:
-        year = item['year']
-        period = item['period']
-        value = item['value']
-        footnotes = ",".join(footnote['text'] for footnote in item['footnotes'] if footnote)
+        for item in series['data']:
+            year = item['year']
+            period = item['period']
+            value = item['value']
+            footnotes = ",".join(footnote['text'] for footnote in item['footnotes'] if footnote)
 
-        # Creation of a dictionary with the common BLS series data labels
-        row_data = {
-            'seriesID': series_id,
-            'year': year,
-            'period': period,
-            'value': value,
-            'footnotes': footnotes,
-        }
+            # Creation of a dictionary with the common BLS series data labels
+            row_data = {
+                'seriesID': series_id,
+                'year': year,
+                'period': period,
+                'value': value,
+                'footnotes': footnotes,
+            }
 
-        parsed_data.append(row_data)
+            parsed_data.append(row_data)
 
-    # Creation of a dataframe for the current series
-    columns = ['seriesID', 'year', 'period', 'value', 'footnotes']
-    data = [[entry.get(i, None) for i in columns] for entry in parsed_data]
-    df = pd.DataFrame(data, columns=columns)
+        # Creation of a data frame for the current series
+        columns = ['seriesID', 'year', 'period', 'value', 'footnotes']
+        data = [[entry.get(i, None) for i in columns] for entry in parsed_data]
+        df = pd.DataFrame(data, columns=columns)
 
-    df['value'] = pd.to_numeric(df['value'])
-    df['month'] = pd.to_numeric(df['period'].replace({'M': ''}, regex=True))
-    df['date'] = pd.to_datetime(df['month'].map(str) + '-' + df['year'].map(str), format='%m-%Y')
-    df = df.sort_values(by=['date'], ascending=True)
+        df['value'] = pd.to_numeric(df['value'])
+        df['month'] = pd.to_numeric(df['period'].replace({'M': ''}, regex=True))
+        df['date'] = pd.to_datetime(df['month'].map(str) + '-' + df['year'].map(str), format='%m-%Y')
+        df = df.sort_values(by=['date'], ascending=True)
 
-    # Reordering of the dataframe's columns
-    df = df[['date', 'value', 'seriesID', 'year', 'month', 'period', 'footnotes']]
+        # Reordering of the data frame's columns
+        df = df[['date', 'value', 'seriesID', 'year', 'month', 'period', 'footnotes']]
 
-    # Resetting the index to start from 0
-    df.reset_index(drop=True, inplace=True)
+        # Resetting the index to start from 0
+        df.reset_index(drop=True, inplace=True)
 
-    # Replacing any empty strings with NaN
-    df.replace('', pd.NA, inplace=True)
+        # Replacing any empty strings with NaN
+        df.replace('', pd.NA, inplace=True)
 
-    # Dropping columns with values of NaN or pd.NA
-    df = df.dropna(axis=1, how='all')
+        # Dropping columns with values of NaN or pd.NA
+        df = df.dropna(axis=1, how='all')
 
-    # Adding the dataframe to the dictionary with the Series ID as the key
-    dataframes_dict[series_id] = df
+        # Adding the data frame to the dictionary with the Series ID as the key
+        dataframes_dict[series_id] = df
+
+    # Return the dictionary of data frames
+    return dataframes_dict
+
+json_data = collect_bls_data()
+dataframes_dict = process_bls_data(json_data)
 
 # Create dataframes for each series
 civ_emp_df = dataframes_dict['LNS12000000']
