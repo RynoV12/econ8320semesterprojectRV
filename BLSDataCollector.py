c18bc02a466866a35c46c6c3b8ebbe6a1f2a2adb
RynoV12 <vilterryan@gmail.com>
2024-12-11 10:39:47 -0600

    Update BLSDataCollector.py
    Edited data displays and added commentary

--- BLSDataCollector.py
+++ BLSDataCollector.py
@@ -5,22 +5,23 @@ import requests
 import json
 
 # Retrieval of series data
-@st.cache_data
+@st.cache_data(ttl = "1d") # Caches the data for 24 hours
 def collect_bls_data():
     headers = {'Content-type': 'application/json'}
-    data = json.dumps({"seriesid": ['LNS12000000', 'LNS13000000', 'LNS14000000', 'CES0000000001'],"startyear":"2023", "endyear":"2024"})
+    data = json.dumps({"seriesid": ['LNS12000000', 'LNS13000000', 'LNS14000000', 'CES0000000001'],"startyear":"2022", "endyear":"2024"})
     p = requests.post('https://api.bls.gov/publicAPI/v2/timeseries/data/', data=data, headers=headers)
     json_data = json.loads(p.text)
     return json_data
 
 # Iterate over the JSON response and extract data from each inputted series
-@st.cache_data
+@st.cache_data(ttl = "1d") # Caches the data for 24 hours
 def process_bls_data(json_data):
     dataframes_dict = {}
     for series in json_data['Results']['series']:
         series_id = series['seriesID']
         parsed_data = []
 
+        # Setting variables equal to collected data frame columns
         for item in series['data']:
             year = item['year']
             period = item['period']
@@ -36,6 +37,7 @@ def process_bls_data(json_data):
                 'footnotes': footnotes,
             }
 
+            # Add row_data dictionary to empty parsed_data list
             parsed_data.append(row_data)
 
         # Creation of a data frame for the current series
@@ -43,6 +45,7 @@ def process_bls_data(json_data):
         data = [[entry.get(i, None) for i in columns] for entry in parsed_data]
         df = pd.DataFrame(data, columns=columns)
 
+        # Changing and reformatting values for columns
         df['value'] = pd.to_numeric(df['value'])
         df['month'] = pd.to_numeric(df['period'].replace({'M': ''}, regex=True))
         df['date'] = pd.to_datetime(df['month'].map(str) + '-' + df['year'].map(str), format='%m-%Y')
@@ -66,6 +69,7 @@ def process_bls_data(json_data):
     # Return the dictionary of data frames
     return dataframes_dict
 
+# Setting variables equal to the results of the collection and processing functions
 json_data = collect_bls_data()
 dataframes_dict = process_bls_data(json_data)
 
@@ -77,48 +81,48 @@ nonfarm_emp_df = dataframes_dict['CES0000000001']
 
 # Addition of title and header in Streamlit
 st.title('ECON 8320 Fall 2024 Semester Project - Ryan Vilter')
-st.header('_:red[U.S.] Bureau of Labor Statistics :blue[Data]_ :flag-us:')
+st.header(':flag-us: _:red[U.S.] Bureau of Labor Statistics :blue[Data]_ :flag-us:')
 
 # Add data frames and labels to my Streamlit dashboard
-st.subheader("Civilian Employment Data Frame", divider = "blue")
+st.subheader("Civilian Employment Data, 2022-2024", divider = "blue")
 st.dataframe(civ_emp_df)
 
-st.subheader("Civilian Unemployment Data Frame", divider = "blue")
+st.subheader("Civilian Unemployment Data, 2022-2024", divider = "blue")
 st.dataframe(civ_unemp_df)
 
-st.subheader("Unemployment Rate Data Frame", divider = "blue")
+st.subheader("Unemployment Rate Data, 2022-2024", divider = "blue")
 st.dataframe(unemp_rt_df)
 
-st.subheader("Nonfarm Employment Data Frame", divider = "blue")
+st.subheader("Nonfarm Employment Data, 2022-2024", divider = "blue")
 st.dataframe(nonfarm_emp_df)
 
-# Plot dataframes as line charts and label them in my Streamlit dashboard
-st.subheader("Civilian Employment Line Chart", divider = "red")
-st.line_chart(civ_emp_df,
+# Plot dataframes as bar charts and label them in my Streamlit dashboard
+st.subheader("Civilian Employment, 2022-2024", divider = "red")
+st.bar_chart(civ_emp_df,
              x='month',
              y='value',
              x_label='Civilian Employment per Month',
              y_label='Number of Employed Civilians',
              color='year')
 
-st.subheader("Civilian Unemployment Line Chart", divider = "red")
-st.line_chart(civ_unemp_df,
+st.subheader("Civilian Unemployment, 2022-2024", divider = "red")
+st.bar_chart(civ_unemp_df,
              x='month',
              y='value',
              x_label='Civilian Unemployment per Month',
              y_label='Number of Unemployed Civilians',
              color='year')
 
-st.subheader("Unemployment Rate Line Chart", divider = "red")
-st.line_chart(unemp_rt_df,
+st.subheader("Unemployment Rate, 2022-2024", divider = "red")
+st.bar_chart(unemp_rt_df,
              x='month',
              y='value',
              x_label='Unemployment Rate per Month',
              y_label='Unemployment Rate',
-            color='year')
+             color='year')
 
-st.subheader("Nonfarm Employment Line Chart", divider = "red")
-st.line_chart(nonfarm_emp_df,
+st.subheader("Nonfarm Employment, 2022-2024", divider = "red")
+st.bar_chart(nonfarm_emp_df,
              x='month',
              y='value',
              x_label='Nonfarm Worker Employment per Month',
